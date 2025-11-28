dd47abe3f904fb0eaac5c5514e309f2be307a4e3
Abdenour Cherfi <abdouch.dev1@gmail.com>
2025-11-28 23:52:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+This file is the main file of pyshield obfuscator
+This is a strong encryption system to protect your files from reverse engineers
+Security Level : Medium (Right now)
+"""
+import os
+import time
+from src.layer1 import Layer1
+from src.layer2 import add_junk_code
+from src.layer3 import Layer3
+
+def main():
+    file_path = input("[>] Drag & drop your .py file here: ").strip().strip('"').strip("'")
+    
+    if not file_path.endswith('.py'):
+        print("Please Choose a python file!")
+
+    if not os.path.isfile(file_path):
+        print("File not found!")
+
+    
+    file_name = file_path.split(".")[-2]
+    file_src = open(file_path, "r").read()
+    symbol = "PYSHIELD__PYSHIELD__PYSHIELD__"
+    layer_1 = Layer1(file_src).layer1_done()
+    layer_2 = add_junk_code(layer_1,symbol)
+    layer_3 = Layer3(layer_2, symbol, file_name).layer3_done()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
+
